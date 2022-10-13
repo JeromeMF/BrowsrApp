9090f4ac2468134b856af0c24e14c6f432ede36f
Jerome Figueiredo <jerome.figueiredo@puregym.com>
2022-10-13 15:24:09 +0100

    Initial Commit

---
+++ BrowsrApp/BrowsrAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrowsrAppApp.swift
+//  BrowsrApp
+//
+//  Created by Jérôme Figueiredo on 13/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct BrowsrAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
